578be3511b38eeff6564a3350c41a9fb0c97fafc
Kohei Esaki <esaki-s@sd.dcns.ne.jp>
2018-01-26 16:40:06 +0900

    Changed x to 51

--- swap.c
+++ swap.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    int x = 34;
+    int x = 51;
     int y = 50;
     
     printf("x is %i\n", x);
@@ -13,7 +13,7 @@ int main(void)
     x = y;
     y = temp;
     int z = 100;
-    
+
     printf("x is now %i\n", x);
     printf("y is now %i\n", y);
 }
